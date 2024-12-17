ab36a13f4ac38572745ce7c4bdd79150e4ab8757
Th1bz <thibaultwks@gmail.com>
2024-12-17 11:25:52 +0100

    Try to fix back in prod

--- migrations/Version20241106103308.php
+++ migrations/Version20241106103308.php
@@ -20,7 +20,7 @@ final class Version20241106103308 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE service_picture (id INT AUTO_INCREMENT NOT NULL, service_id INT NOT NULL, picture_data VARCHAR(255) DEFAULT NULL, INDEX IDX_D95F1D15ED5CA9E6 (service_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE service_picture (id INT AUTO_INCREMENT NOT NULL, service_id INT NOT NULL, picture_data VARCHAR(255) DEFAULT NULL, INDEX IDX_D95F1D15ED5CA9E6 (service_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE service_picture ADD CONSTRAINT FK_D95F1D15ED5CA9E6 FOREIGN KEY (service_id) REFERENCES service (id)');
     }
 
@@ -28,6 +28,6 @@ final class Version20241106103308 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE service_picture DROP FOREIGN KEY FK_D95F1D15ED5CA9E6');
-        $this->addSql('DROP TABLE service_picture');
+        $this->addSql('ALTER TABLE service_picture');
     }
 }
